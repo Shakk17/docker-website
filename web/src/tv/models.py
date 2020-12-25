c80840d799cef857fad8f3a038882f5e9272e9d5
Alessandro Pina <pina.alessandro17@gmail.com>
2020-12-25 11:34:00 +0100

    Overview + IMDb button addition

--- web/src/tv/models.py
+++ web/src/tv/models.py
@@ -5,7 +5,8 @@ class Imdb(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=20)
-
+    overview = models.CharField(max_length=1000)
+    
     rating_avg = models.DecimalField(max_digits=2, decimal_places=2)
     prediction = models.DecimalField(max_digits=2, decimal_places=2)
 
